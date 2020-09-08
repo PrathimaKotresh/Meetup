fd8145c081aa44d935b0fb0fbbfce7c7897fb196
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-09-08 22:01:24 +0200

    Updates

---
+++ precache-manifest.e0ff26c7731fc4a5a299684f54eb4a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d53327179441cc9e6a931ead83026d",
+    "url": "/Meetup/index.html"
+  },
+  {
+    "revision": "841b1e4435e43913f42e",
+    "url": "/Meetup/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa44b4cd9222c00b0b53",
+    "url": "/Meetup/static/js/2.e3215a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meetup/static/js/2.e3215a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841b1e4435e43913f42e",
+    "url": "/Meetup/static/js/main.f85dbf82.chunk.js"
+  },
+  {
+    "revision": "55380c6c01a6cf92f71e",
+    "url": "/Meetup/static/js/runtime-main.2ef098fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Meetup/static/media/logo.5d5d9eef.svg"
+  }
+]);
