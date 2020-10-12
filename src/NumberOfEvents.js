0e57e0e4c43e8c70d7f2545c04080edafd7afed1
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-10-12 16:40:17 +0200

    alert messages

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
-    errorMessage: null
+    errorText: null
   };
 
   handleOnChange = (event) => {
     const value = event.target.value;
-    if (value < 1) {
-      this.setState({
-        errorMessage: "Please enter vaid number",
-        numberOfEvents: null
-      })
-    }
-    else {
-      this.setState({
-        errorMessage: null,
-        numberOfEvents: value
-      })
-    }
+    if (value < 1) this.setState({ errorText: "Please enter a valid number" });
+    else this.setState({ errorText: "" });
+    this.setState({ numberOfEvents: value });
     this.props.updateEvents(null, null, value);
-  }
+  };
 
   render() {
     return (
       <div className="numberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
         <label className="numberOfEvents-label">Show number of Events</label>
         <input value={this.state.numberOfEvents} className="numberOfEvents-number" onChange={this.handleOnChange} />
         {this.state.errorMessage && <p className="errorMessage">{this.state.errorMessage}</p>}
